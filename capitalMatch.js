0c9b01d3bcba6c1191a0833e9bd1815ac8f070b5
genelin <genelin825@hotmail.com>
2023-04-10 21:09:46 -0400

    updated the general structure of the project

--- capitalMatch.js
+++ capitalMatch.js
@@ -9,16 +9,45 @@ let gameOn = true;
 
 
 // check if there are inputs
+    
+} else {
+
+}
+
+
+function figletPrint(text) {
+    figlet(text, function (err, data) {
+        if (err) {
+          console.log("error with figlet output");
+          console.dir(err);
+          return;
+        }
+        console.log(data);
+    });
+}
+
+// check if country provided can be found in the database
+function validCountry (country) {
+}
+
+// function to check that the country and capital passed match each other
+function checkAnswer (country, capital) {
+    //console.log(clm.getCountryByName(process.argv[2]).capital);
+    if (clm.getCountryByName(process.argv[2]).capital === process.argv[3]) {
+        console.log ("Correct!!!");
+    }
+}
+
+// function to generate a random country suggestion for the next game
+function generateSuggestion() {
+
+}
 
 
 
 
 
 
-console.log(clm.getCountryByName(process.argv[2]).capital);
-if (clm.getCountryByName(process.argv[2]).capital === process.argv[3]) {
-    console.log ("Correct!!!")
-};
 
 
 
